09ad0fa22c257675ed2e17e1e206552db26b3035
brownard <brownard@live.co.uk>
2016-03-26 13:00:01 +0000

    Capitalise guids and update default groups

--- MediaPortal/Incubator/HomeEditor/Groups/DefaultGroups.cs
+++ MediaPortal/Incubator/HomeEditor/Groups/DefaultGroups.cs
@@ -14,56 +14,58 @@ namespace HomeEditor.Groups
     {
       return new List<HomeMenuGroup>
       {
-        new HomeMenuGroup("[Media.ImagesMenuItem]", new Guid("e9463404-ff36-4255-91fd-4742ecdbaf6a"))
+        new HomeMenuGroup("[Media.ImagesMenuItem]", new Guid("E9463404-FF36-4255-91FD-4742ECDBAF6A"))
         {
           Actions = new List<HomeMenuAction>
           {
-            new HomeMenuAction("[Media.ImagesMenuItem]", new Guid("55556593-9fe9-436c-a3b6-a971e10c9d44"))
+            new HomeMenuAction("[Media.ImagesMenuItem]", new Guid("55556593-9FE9-436C-A3B6-A971E10C9D44"))
           }
         },
-        new HomeMenuGroup("[Media.AudioMenuItem]", new Guid("b527e507-2b32-437d-8cd7-d670950bad39"))
+        new HomeMenuGroup("[Media.AudioMenuItem]", new Guid("B527E507-2B32-437D-8CD7-D670950BAD39"))
         {
           Actions = new List<HomeMenuAction>
           {
-            new HomeMenuAction("[Media.AudioMenuItem]", new Guid("30715d73-4205-417f-80aa-e82f0834171f")),
-            new HomeMenuAction("[PartyMusicPlayer.MenuItem]", new Guid("e00b8442-8230-4d7b-b871-6ac77755a0d5"))
+            new HomeMenuAction("[Media.AudioMenuItem]", new Guid("30715D73-4205-417F-80AA-E82F0834171F")),
+            new HomeMenuAction("[PartyMusicPlayer.MenuItem]", new Guid("E00B8442-8230-4D7B-B871-6AC77755A0D5"))
           }
         },
-        new HomeMenuGroup("[Media.VideosMenuItem]", new Guid("99b80a99-419c-4fa8-97e1-1b4e6406e09a"))
+        new HomeMenuGroup("[Media.VideosMenuItem]", new Guid("99B80A99-419C-4FA8-97E1-1B4E6406E09A"))
         {
           Actions = new List<HomeMenuAction>
           {
-            new HomeMenuAction("[Media.VideosMenuItem]", new Guid("a4df2df6-8d66-479a-9930-d7106525eb07")),
-            new HomeMenuAction("[Media.MoviesMenuItem]", new Guid("80d2e2cc-baaa-4750-807b-f37714153751")),
-            new HomeMenuAction("[Media.SeriesMenuItem]", new Guid("30f57cba-459c-4202-a587-09fff5098251")),
-            new HomeMenuAction("OnlineVideos", new Guid("c33e39cc-910e-41c8-bffd-9eccd340b569")),
-            new HomeMenuAction("[Media.BrowseMediaMenuItem]", new Guid("93442df7-186d-42e5-a0f5-cf1493e68f49"))
+            new HomeMenuAction("[Media.VideosMenuItem]", new Guid("A4DF2DF6-8D66-479A-9930-D7106525EB07")),
+            new HomeMenuAction("[Media.MoviesMenuItem]", new Guid("80D2E2CC-BAAA-4750-807B-F37714153751")),
+            new HomeMenuAction("[Media.SeriesMenuItem]", new Guid("30F57CBA-459C-4202-A587-09FFF5098251")),
+            new HomeMenuAction("OnlineVideos", new Guid("C33E39CC-910E-41C8-BFFD-9ECCD340B569")),
+            new HomeMenuAction("[Media.BrowseMediaMenuItem]", new Guid("93442DF7-186D-42E5-A0F5-CF1493E68F49"))
           }
         },
-        new HomeMenuGroup("[SlimTvClient.Main]", new Guid("c556242c-a97d-4947-9821-b3e71f866836"))
+        new HomeMenuGroup("[SlimTvClient.Main]", new Guid("C556242C-A97D-4947-9821-B3E71F866836"))
         {
           Actions = new List<HomeMenuAction>
           {
-            new HomeMenuAction("[SlimTvClient.Main]", new Guid("b4a9199f-6dd4-4bda-a077-de9c081f7703")),
-            new HomeMenuAction("[SlimTvClient.TvGuide]", new Guid("a298dfbe-9da8-4c16-a3ea-a9b354f3910c")),
-            new HomeMenuAction("[SlimTvClient.Schedules]", new Guid("87355e05-a15b-452a-85b8-98d4fc80034e")),
-            new HomeMenuAction("[SlimTvClient.RecordingsMenuItem]", new Guid("7f52d0a1-b7f8-46a1-a56b-1110bbfb7d51")),
-            new HomeMenuAction("[SlimTvClient.ProgramSearch]", new Guid("d91738e9-3f85-443b-abbd-ef01731734ad"))
+            new HomeMenuAction("[SlimTvClient.Main]", new Guid("B4A9199F-6DD4-4BDA-A077-DE9C081F7703")),
+            new HomeMenuAction("[SlimTvClient.TvGuide]", new Guid("A298DFBE-9DA8-4C16-A3EA-A9B354F3910C")),
+            new HomeMenuAction("[SlimTvClient.Schedules]", new Guid("87355E05-A15B-452A-85B8-98D4FC80034E")),
+            new HomeMenuAction("[SlimTvClient.RecordingsMenuItem]", new Guid("7F52D0A1-B7F8-46A1-A56B-1110BBFB7D51")),
+            new HomeMenuAction("[SlimTvClient.ProgramSearch]", new Guid("D91738E9-3F85-443B-ABBD-EF01731734AD"))
           }
         },
-        new HomeMenuGroup("[News.Title]", new Guid("f41d7acb-ea54-42d3-993c-e9770762931f"))
+        new HomeMenuGroup("[News.Title]", new Guid("F41D7ACB-EA54-42D3-993C-E9770762931F"))
         {
           Actions = new List<HomeMenuAction>
           {
-            new HomeMenuAction("[News.Title]", new Guid("bb49a591-7705-408f-8177-45d633fdfad0")),
-            new HomeMenuAction("[Weather.Title]", new Guid("e34fdb62-1f3e-4aa9-8a61-d143e0af77b5"))
+            new HomeMenuAction("[News.Title]", new Guid("BB49A591-7705-408F-8177-45D633FDFAD0")),
+            new HomeMenuAction("[Weather.Title]", new Guid("E34FDB62-1F3E-4AA9-8A61-D143E0AF77B5"))
           }
         },
-        new HomeMenuGroup("[Configuration.Name]", new Guid("df5a84b0-2de1-4dd2-b0bc-982751db9eeb"))
+        new HomeMenuGroup("[Configuration.Name]", new Guid("DF5A84B0-2DE1-4DD2-B0BC-982751DB9EEB"))
         {
           Actions = new List<HomeMenuAction>
           {
-            new HomeMenuAction("[Configuration.Name]", new Guid("f6255762-c52a-4231-9e67-14c28735216e"))
+            new HomeMenuAction("[Configuration.Name]", new Guid("F6255762-C52A-4231-9E67-14C28735216E")),
+            new HomeMenuAction("[SystemState.ShutdownDialogStateDisplayLabel]", new Guid("C5B14E9C-7F54-4B75-BEB0-8BE2EA1F4394")),
+            new HomeMenuAction("[HomeEditor.Configuration]", new Guid("AEBAE6B5-71B3-4164-992F-296E2ACF8E5C"))
           }
         },
       };
